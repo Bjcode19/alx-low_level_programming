dcb566ac00332af68c5cdd4598eca69a981bcf87
Bjcode19 <sugarbhoy18@gmail.com>
2023-09-11 09:48:02 -0700

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < o; o++)
+	for (i = 0; i < o; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }

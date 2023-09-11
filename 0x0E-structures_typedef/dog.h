7fdd5e419c7714074bd67037ef928082ba4cc093
Bjcode19 <sugarbhoy18@gmail.com>
2023-09-11 08:26:58 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,17 +10,14 @@
  * Description: first struct with alx
  */
 
-/**
- * dog_t - new name for struct dog
- */
-typedef struct dog dog_t;
+typedef struct dog
 
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
